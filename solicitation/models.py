e49e9dd3f81d95b5eebfd17491be27be22ed3b2b
Fecef <fecef.figueiredo@gmail.com>
2023-10-07 20:13:13 -0300

    feat: add additional entity field

--- solicitation/models.py
+++ solicitation/models.py
@@ -3,12 +3,26 @@ import uuid
 from django.db import models
 
 
+class ModalityType(models.TextChoices):
+    CC = "Cartão de Crédito"
+    CCON = "Crédito Consignado"
+
+
+class StreamType(models.TextChoices):
+    AUTO = "Automático"
+    MANUAL = "Manual"
+
+
 class Solicitation(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     installments = models.PositiveSmallIntegerField()
     installment_value = models.DecimalField(max_digits=12, decimal_places=2)
     loan_to_get = models.DecimalField(max_digits=12, decimal_places=2)
     loan_to_pay = models.DecimalField(max_digits=12, decimal_places=2)
+    modality = models.CharField(max_length=20, choices=ModalityType.choices)
+    stream = models.CharField(max_length=20, choices=StreamType.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     client = models.OneToOneField("client.Client", on_delete=models.CASCADE)
     rate_table = models.OneToOneField("rate_table.RateTable", on_delete=models.CASCADE)
--- solicitation/serializers.py
+++ solicitation/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-
+from datetime import datetime
 from .models import Solicitation
 
 
 class SolicitationSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
     class Meta:
         model = Solicitation
         fields = [
@@ -12,6 +15,22 @@ class SolicitationSerializer(serializers.ModelSerializer):
             "installment_value",
             "loan_to_get",
             "loan_to_pay",
+            "modality",
+            "stream",
             "client",
             "rate_table",
+            "created_at",
+            "updated_at",
         ]
+
+    def get_created_at(self, obj: Solicitation):
+        now = datetime.now()
+        obj.created_at = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        return obj.created_at
+
+    def get_updated_at(self, obj: Solicitation):
+        now = datetime.now()
+        obj.updated_at = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        return obj.updated_at
